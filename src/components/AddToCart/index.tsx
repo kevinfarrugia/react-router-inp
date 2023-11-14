682eb5289678116abad9f9d797e448726255c308
Kevin Farrugia <hello@imkev.dev>
2023-11-14 12:17:25 +0100

    Create test case

---
+++ src/components/AddToCart/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css";
+
+function AddToCart() {
+  return <input type="button" value="Add to Cart" className={styles.button} />;
+}
+
+export default AddToCart;
--- src/components/DataList/index.tsx
+++ src/components/DataList/index.tsx
@@ -1,3 +1,4 @@
+import AddToCart from "../AddToCart";
 import styles from "./styles.module.css";
 
 function DataList() {
@@ -5,31 +6,25 @@ function DataList() {
     <div className={styles.section}>
       <h1 className={styles.heading}>Lorem Ipsum</h1>
       <ul className={styles.list}>
-        <li className={styles.item}>
-          <h3 className={styles.itemTitle}>Lorem ipsum dolor sit amet</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, quis
-            aliquam nosl eget ultricies aliquam, nunc nisl aliquet nunc, quis
-            aliquam no.
-          </p>
-        </li>
-        <li className={styles.item}>
-          <h3 className={styles.itemTitle}>Lorem ipsum</h3>
-          <p>
-            Lorem ipsum dolor sit sl eget ultricies aliquam, nunc nisl aliquet
-            nunc, quis amet, consectetur adipiscing elit. Nullam euismod, nisl
-            eget ultricies aliquam, nunc nisl aliquet nunc, quis aliquam no.
-          </p>
-        </li>
-        <li className={styles.item}>
-          <h3 className={styles.itemTitle}>Lorem ipsum amet</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, quis
-            aliquam no.
-          </p>
-        </li>
+        {[...new Array(500)].map((_, index) => (
+          <li key={index} className={styles.item}>
+            <h3 className={styles.itemTitle}>Lorem ipsum dolor sit amet</h3>
+            <img
+              src="http://placekitten.com/200/300"
+              alt="Image of a kitten"
+              width="200"
+              height="300"
+              className={styles.thumbnail}
+            />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, quis
+              aliquam nosl eget ultricies aliquam, nunc nisl aliquet nunc, quis
+              aliquam no.
+            </p>
+            <AddToCart />
+          </li>
+        ))}
       </ul>
     </div>
   );
