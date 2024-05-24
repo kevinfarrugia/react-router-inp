a2f3218d7e7d6e075f84ab7818290d7105c1406a
Kevin Farrugia <hello@imkev.dev>
2024-05-24 14:26:17 +0200

    Only render one item to reduce style and layout but iterate all 500 items

--- src/components/DataList/index.tsx
+++ src/components/DataList/index.tsx
@@ -8,28 +8,30 @@ function DataList({ title = "" }) {
       <ul className={styles.list}>
         {Array.from({ length: 500 }, () =>
           Math.floor(Math.random() * 1000)
-        ).map((n, index) => (
-          <li key={index} className={styles.item}>
-            <h3 className={styles.itemTitle}>
-              {n}. Lorem ipsum dolor sit amet
-            </h3>
-            <img
-              src="http://placekitten.com/200/300"
-              alt="Image of a kitten"
-              width="200"
-              height="300"
-              className={styles.thumbnail}
-              loading={index > 1 ? "lazy" : undefined}
-            />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, quis
-              aliquam nosl eget ultricies aliquam, nunc nisl aliquet nunc, quis
-              aliquam no.
-            </p>
-            <AddToCart />
-          </li>
-        ))}
+        ).map((n, index) =>
+          index === 0 ? (
+            <li key={index} className={styles.item}>
+              <h3 className={styles.itemTitle}>
+                {n}. Lorem ipsum dolor sit amet
+              </h3>
+              <img
+                src="http://placekitten.com/200/300"
+                alt="Image of a kitten"
+                width="200"
+                height="300"
+                className={styles.thumbnail}
+                loading={index > 1 ? "lazy" : undefined}
+              />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc,
+                quis aliquam nosl eget ultricies aliquam, nunc nisl aliquet
+                nunc, quis aliquam no.
+              </p>
+              <AddToCart />
+            </li>
+          ) : null
+        )}
       </ul>
     </div>
   );
